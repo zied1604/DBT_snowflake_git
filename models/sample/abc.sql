59ebc564daead0f8e7fa6c4d0153eaaeaabe3615
nas.zied@yahoo.com <nas.zied@yahoo.com>
2024-07-06 20:45:02 +0000

    commit create table

---
+++ models/sample/abc.sql
@@ -0,0 +1,5 @@
+select 'John' as first_name, 'DOE' as last_name, '1970-01-01' as birthdate
+UNION ALL
+select 'Jane' as first_name, 'Smith' as last_name, '1985-03-15' as birthdate
+UNION ALL
+select 'Bob' as first_name, 'Jhonson' as last_name, '1978-11-22' as birthdate
